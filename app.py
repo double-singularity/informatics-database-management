7cdb1a65e8d148c5e22e720e08183fcb11e7a752
double-singularity <rstu1081@gmail.com>
2024-05-30 15:02:15 +0700

    Add display route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-import pymysql
+from db import Database
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12345678"
@@ -8,3 +8,9 @@ app.config["SECRET_KEY"] = "12345678"
 @app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("index.html")
+
+test = Database()
+
+@app.route("/display", methods=["POST", "GET"])
+def display():
+    return render_template("display.html", my_list=test.exec("SELECT * FROM mahasiswa"))
