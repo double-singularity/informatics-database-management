a72feb1aea9caab15ec335ec474e3db7989324eb
double-singularity <rstu1081@gmail.com>
2024-05-31 13:36:24 +0700

    Add view table prototype

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app.config["SECRET_KEY"] = "12345678"
 
 db = Database()
 
+def mat_print(mat):
+    for thing in mat:
+        print(thing)
+
 @app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("index.html")
@@ -18,6 +22,15 @@ def administrator():
 def homepage():
     return "<h1>this is homepage</h1>"
 
-@app.route("/display", methods=["POST", "GET"])
-def display():
-    return render_template("display.html", my_list=db.exec("SELECT * FROM mahasiswa"))
+@app.route("/display/<table_name>", methods=["POST", "GET"])
+def display(table_name):
+    return render_template("display.html", my_list=db.exec(f"SELECT * FROM {table_name}"))
+
+@app.route('/view/<table_name>')
+def view_table(table_name):
+    columns = db.exec(f"DESCRIBE {table_name}")
+    first_columns = []
+    for thing in columns:
+        first_columns.append(thing[0])
+    print(first_columns)
+    return render_template('view_table.html', table_name=table_name, rows=db.exec(f"SELECT * FROM {table_name}"), first_columns=first_columns)
