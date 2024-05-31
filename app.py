3f990199eceb83d4f7f04db9aa85ce9672f53b78
double-singularity <rstu1081@gmail.com>
2024-05-31 23:20:57 +0700

    Tidying code

--- app.py
+++ app.py
@@ -4,23 +4,29 @@ from db import Database
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12345678"
 
+
 db = Database()
+
 
 def mat_print(mat):
     for thing in mat:
         print(thing)
 
+
 @app.route("/", methods=["POST", "GET"])
 def home():
     return render_template("index.html")
+
 
 @app.route("/administrator")
 def administrator():
     return render_template("administrator.html")
 
+
 @app.route("/homepage")
 def homepage():
     return "<h1>this is homepage</h1>"
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -34,5 +40,8 @@ def view_table(table_name):
     first_columns = []
     for thing in columns:
         first_columns.append(thing[0])
-    print(first_columns)
-    return render_template('view_table.html', table_name=table_name, rows=db.exec(f"SELECT * FROM {table_name}"), first_columns=first_columns)
+    return render_template('view_table.html', 
+                           table_name=table_name, 
+                           rows=db.exec(f"SELECT * FROM {table_name}"), 
+                           first_columns=first_columns
+                           )
